c6c13c1222623160ebd81de6de6aca444aa526bf
David <dmg_92@hotmail.com>
2024-11-06 14:16:55 -0600

    "whitenoise.middleware.WhiteNoiseMiddleware",

--- pos_multi_store/settings.py
+++ pos_multi_store/settings.py
@@ -49,6 +49,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
