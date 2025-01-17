ac02b49fa0b5dc381524345796c0e3669dd15c65
David <dmg_92@hotmail.com>
2025-01-16 20:11:34 -0600

    sales product

--- sales/models.py
+++ sales/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
 from clients.models import Client
 from products.models import Product, Store
 
@@ -16,8 +15,8 @@ class Sale(models.Model):
 
 
 class SaleProduct(models.Model):
-    sale = models.ForeignKey(Sale, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    sale = models.ForeignKey(Sale, on_delete=models.CASCADE, related_name='products')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='sales')
     quantity = models.PositiveIntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
