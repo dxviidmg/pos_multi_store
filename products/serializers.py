2ad5a4bb65dda7a376d6ec1ad5166a0d903b02f5
David <dmg_92@hotmail.com>
2025-02-21 08:52:35 -0600

    cash summary

--- products/serializers.py
+++ products/serializers.py
@@ -1,215 +1,220 @@
 from rest_framework import serializers
-from .models import Product, StoreProduct, Transfer, Store, Brand, StoreProductLog, CashFlow
+from .models import (
+    Product,
+    StoreProduct,
+    Transfer,
+    Store,
+    Brand,
+    StoreProductLog,
+    CashFlow,
+)
 from django.core.exceptions import ValidationError
+from sales.cash_summary_utils import calculate_cash_summary
+from datetime import date
+
 
 
 class BrandSerializer(serializers.ModelSerializer):
-	product_count = serializers.SerializerMethodField()
+    product_count = serializers.SerializerMethodField()
 
-	def get_product_count(self, obj):
-		return obj.get_product_count()
+    def get_product_count(self, obj):
+        return obj.get_product_count()
 
-	class Meta:
-		model = Brand
-		exclude = ["tenant"]
+    class Meta:
+        model = Brand
+        exclude = ["tenant"]
 
 
 class StoreProductBaseSerializer(serializers.ModelSerializer):
-	product_code = serializers.SerializerMethodField()
-	product_brand = serializers.SerializerMethodField()
-	product_name = serializers.SerializerMethodField()
+    product_code = serializers.SerializerMethodField()
+    product_brand = serializers.SerializerMethodField()
+    product_name = serializers.SerializerMethodField()
 
-	def get_product_code(self, obj):
-		return obj.product.code
+    def get_product_code(self, obj):
+        return obj.product.code
 
-	def get_product_brand(self, obj):
-		return obj.product.brand.name
-	
-	def get_product_name(self, obj):
-		return obj.product.name
-		
+    def get_product_brand(self, obj):
+        return obj.product.brand.name
 
+    def get_product_name(self, obj):
+        return obj.product.name
 
-	class Meta:
-		model = StoreProduct
-		fields = "__all__"
-
+    class Meta:
+        model = StoreProduct
+        fields = "__all__"
 
 
 class StoreProductSerializer(StoreProductBaseSerializer):
-	product_id = serializers.SerializerMethodField()
-	product_description = serializers.SerializerMethodField()
-	available_stock = serializers.SerializerMethodField()
-	reserved_stock = serializers.SerializerMethodField()
+    product_id = serializers.SerializerMethodField()
+    product_description = serializers.SerializerMethodField()
+    available_stock = serializers.SerializerMethodField()
+    reserved_stock = serializers.SerializerMethodField()
 
-	prices = serializers.SerializerMethodField()
-	stock_in_other_stores = serializers.SerializerMethodField()
+    prices = serializers.SerializerMethodField()
+    stock_in_other_stores = serializers.SerializerMethodField()
 
-	def get_product_id(self, obj):
-		return obj.product.id
+    def get_product_id(self, obj):
+        return obj.product.id
 
+    def get_product_description(self, obj):
+        return obj.product.get_description()
 
-	def get_product_description(self, obj):
-		return obj.product.get_description()
+    def get_available_stock(self, obj):
+        return obj.calculate_available_stock()
 
-	def get_available_stock(self, obj):
-		return obj.calculate_available_stock()
+    def get_reserved_stock(self, obj):
+        return obj.calculate_reserved_stock()
 
-	def get_reserved_stock(self, obj):
-		return obj.calculate_reserved_stock()
-	
+    def get_prices(self, obj):
+        return {
+            "unit_price": obj.product.unit_price,
+            "wholesale_price": obj.product.wholesale_price,
+            "min_wholesale_quantity": obj.product.min_wholesale_quantity,
+            "apply_wholesale": obj.product.apply_wholesale(),
+            "wholesale_price_on_client_discount": obj.product.wholesale_price_on_client_discount,
+        }
 
-	def get_prices(self, obj):
-		return {
-			"unit_price": obj.product.unit_price,
-			"wholesale_price": obj.product.wholesale_price,
-			"min_wholesale_quantity": obj.product.min_wholesale_quantity,
-			"apply_wholesale": obj.product.apply_wholesale(),
-			"wholesale_price_on_client_discount": obj.product.wholesale_price_on_client_discount, 
-		
-		}
-
-	def get_stock_in_other_stores(self, obj):
-		# Optimize by pre-filtering and reducing unnecessary calculations
-		return [
-			{
-				"store_id": str(sp.store.id),
-				"store_name": str(sp.store),
-				"available_stock": sp.calculate_available_stock(),
-			}
-			for sp in StoreProduct.objects.filter(product=obj.product).exclude(id=obj.id).exclude(stock=0)
-			if sp.calculate_available_stock() > 0
-		]
-	
+    def get_stock_in_other_stores(self, obj):
+        # Optimize by pre-filtering and reducing unnecessary calculations
+        return [
+            {
+                "store_id": str(sp.store.id),
+                "store_name": str(sp.store),
+                "available_stock": sp.calculate_available_stock(),
+            }
+            for sp in StoreProduct.objects.filter(product=obj.product)
+            .exclude(id=obj.id)
+            .exclude(stock=0)
+            if sp.calculate_available_stock() > 0
+        ]
 
 
 class StoreProductLogSerializer(serializers.ModelSerializer):
 
-	description = serializers.SerializerMethodField()
-	difference = serializers.SerializerMethodField()
-	user_username = serializers.SerializerMethodField()
+    description = serializers.SerializerMethodField()
+    difference = serializers.SerializerMethodField()
+    user_username = serializers.SerializerMethodField()
 
-	def get_description(self, obj):
-		return obj.get_description()
+    def get_description(self, obj):
+        return obj.get_description()
 
-	def get_difference(self, obj):
-		return obj.calculate_difference()
-	
-	def get_user_username(self, obj):
-		return obj.user.username
+    def get_difference(self, obj):
+        return obj.calculate_difference()
 
-	class Meta:
-		model = StoreProductLog
-		fields = "__all__"
+    def get_user_username(self, obj):
+        return obj.user.username
+
+    class Meta:
+        model = StoreProductLog
+        fields = "__all__"
 
 
 class StoreProductLogSerializer2(StoreProductLogSerializer):
 
-	product_brand = serializers.SerializerMethodField()
-	product_name = serializers.SerializerMethodField()
+    product_brand = serializers.SerializerMethodField()
+    product_name = serializers.SerializerMethodField()
 
-	def get_product_brand(self, obj):
-		return obj.store_product.product.brand.name
+    def get_product_brand(self, obj):
+        return obj.store_product.product.brand.name
 
-	def get_product_name(self, obj):
-		return obj.store_product.product.name
-	
+    def get_product_name(self, obj):
+        return obj.store_product.product.name
+
 
 class TransferSerializer(serializers.ModelSerializer):
-	product_code = serializers.SerializerMethodField()
-	product_description = serializers.SerializerMethodField()
-	description = serializers.SerializerMethodField()
+    product_code = serializers.SerializerMethodField()
+    product_description = serializers.SerializerMethodField()
+    description = serializers.SerializerMethodField()
 
-	def get_product_code(self, obj):
-		return obj.product.code
+    def get_product_code(self, obj):
+        return obj.product.code
 
-	def get_product_description(self, obj):
-		return obj.product.get_description()
+    def get_product_description(self, obj):
+        return obj.product.get_description()
 
-	def get_description(self, obj):
-		store = self.context["request"].store
-		if store == obj.origin_store:
-			return "Le proveere este producto a " + obj.destination_store.__str__()
-		elif store == obj.destination_store:
-			return "Le solicite este producto a " + obj.origin_store.__str__()
-		return "No tengo gerencia entre traspaso"
+    def get_description(self, obj):
+        store = self.context["request"].store
+        if store == obj.origin_store:
+            return "Le proveere este producto a " + obj.destination_store.__str__()
+        elif store == obj.destination_store:
+            return "Le solicite este producto a " + obj.origin_store.__str__()
+        return "No tengo gerencia entre traspaso"
 
-	class Meta:
-		model = Transfer
-		fields = "__all__"
+    class Meta:
+        model = Transfer
+        fields = "__all__"
 
 
 class StoreSerializer(serializers.ModelSerializer):
-	full_name = serializers.SerializerMethodField()
-	store_type_display = serializers.SerializerMethodField()
-	investment = serializers.SerializerMethodField()
-	profit_today = serializers.SerializerMethodField()
+    full_name = serializers.SerializerMethodField()
+    store_type_display = serializers.SerializerMethodField()
+    investment = serializers.SerializerMethodField()
+    cash_summary = serializers.SerializerMethodField()
 
-	def get_full_name(self, obj):
-		return obj.get_full_name()
+    def get_full_name(self, obj):
+        return obj.get_full_name()
 
-	def get_store_type_display(self, obj):
-		return obj.get_store_type_display()
+    def get_store_type_display(self, obj):
+        return obj.get_store_type_display()
 
-	def get_investment(self, obj):
-		return obj.get_investment()
-	
-	def get_profit_today(self, obj):
-		return obj.get_profit_today()
-	
-	
-	class Meta:
-		model = Store
-		fields = "__all__"
+    def get_investment(self, obj):
+        return obj.get_investment()
+
+    def get_cash_summary(self, obj):
+        today = date.today() 
+        return calculate_cash_summary(obj, today)
+
+    class Meta:
+        model = Store
+        fields = "__all__"
 
 
 class ProductSerializer(serializers.ModelSerializer):
 
-	brand_name = serializers.SerializerMethodField()
-	apply_wholesale = serializers.SerializerMethodField()
+    brand_name = serializers.SerializerMethodField()
+    apply_wholesale = serializers.SerializerMethodField()
 
-	def get_brand_name(self, obj):
-		return obj.brand.name
+    def get_brand_name(self, obj):
+        return obj.brand.name
 
-	def get_apply_wholesale(self, obj):
-		return obj.apply_wholesale()
+    def get_apply_wholesale(self, obj):
+        return obj.apply_wholesale()
 
-	class Meta:
-		model = Product
-		fields = "__all__"
+    class Meta:
+        model = Product
+        fields = "__all__"
 
-	def validate(self, data):
-		request = self.context.get("request")
-		method = request.method if request else None
+    def validate(self, data):
+        request = self.context.get("request")
+        method = request.method if request else None
 
-		if method == "POST":
-			if Product.objects.filter(
-				code=data["code"], brand__tenant=data["brand"].tenant
-			).exists():
-				raise ValidationError(
-					{"code": "product with this code already exists."}
-				)
+        if method == "POST":
+            if Product.objects.filter(
+                code=data["code"], brand__tenant=data["brand"].tenant
+            ).exists():
+                raise ValidationError(
+                    {"code": "product with this code already exists."}
+                )
 
-		return data
+        return data
 
 
 class CashFlowSerializer(serializers.ModelSerializer):
-	transaction_type_display = serializers.SerializerMethodField()
-	user_username = serializers.SerializerMethodField()
+    transaction_type_display = serializers.SerializerMethodField()
+    user_username = serializers.SerializerMethodField()
 
-	def get_transaction_type_display(self, obj):
-		return obj.get_transaction_type_display()
+    def get_transaction_type_display(self, obj):
+        return obj.get_transaction_type_display()
 
-	def get_user_username(self, obj):
-		return obj.user.username
+    def get_user_username(self, obj):
+        return obj.user.username
 
-
-	class Meta:
-		model = CashFlow
-		fields = "__all__"
+    class Meta:
+        model = CashFlow
+        fields = "__all__"
 
 
 class CashFlowCreateSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = CashFlow
-		fields = ["concept", "amount", "transaction_type"]
+    class Meta:
+        model = CashFlow
+        fields = ["concept", "amount", "transaction_type"]
---
+++ sales/cash_summary_utils.py
@@ -0,0 +1,120 @@
+from datetime import date
+from django.db.models import Sum
+from collections import defaultdict
+from .models import Sale, Payment
+from products.models import CashFlow
+
+def calculate_cash_summary(store, date):  
+    # Obtener totales de CashFlow agrupados por tipo de transacción
+    cash_flow_totals_by_type = (
+        CashFlow.objects.filter(store=store, created_at__date=date)
+        .values("transaction_type")
+        .annotate(total=Sum("amount"))
+    )
+
+    # Mapear los totales en un defaultdict para evitar valores None
+    transaction_sums = defaultdict(
+        int,
+        {entry["transaction_type"]: entry["total"] for entry in cash_flow_totals_by_type},
+    )
+    total_income = transaction_sums["E"]
+    total_expenses = transaction_sums["S"]
+    net_cash_flow = total_income - total_expenses
+
+    # Obtener total de ventas
+    sales = Sale.objects.filter(store=store, created_at__date=date)
+    total_sales = sales.aggregate(total=Sum("total"))["total"] or 0
+
+    # Calcular la ganancia total del día sumando el beneficio de cada venta
+    total_profit = sum(sale.get_profit() for sale in sales)
+
+    # Obtener pagos relacionados con esas ventas
+    related_payments = Payment.objects.filter(
+        sale__store=store, sale__created_at__date=date
+    )
+    payments_grouped_by_method = related_payments.values("payment_method").annotate(
+        total_amount=Sum("amount")
+    )
+    total_received_payments = (
+        related_payments.aggregate(total=Sum("amount"))["total"] or 0
+    )
+
+        # Mapeo de métodos de pago
+    payment_method_labels = dict(Payment.PAYMENT_METHOD_CHOICES)
+
+    # Crear un diccionario con todos los métodos de pago y valores iniciales en 0
+    payments_dict = {method: 0 for method in payment_method_labels.keys()}
+
+    # Actualizar con los valores reales obtenidos de la base de datos
+    for payment in payments_grouped_by_method:
+        payments_dict[payment["payment_method"]] = payment["total_amount"]
+
+    # Construcción de la respuesta, asegurando que todos los métodos estén presentes
+    cash_summary = [
+        {
+            "name": payment_method_labels.get(method, method),
+            "amount": amount,
+            "payment_method_data": True,
+        }
+        for method, amount in payments_dict.items()
+    ]
+
+
+    net_cash = (
+        next(
+            (item for item in cash_summary if item["name"] == "Efectivo"),
+            {"amount": 0},
+        )["amount"]
+        - total_expenses
+    )
+
+    cash_summary.extend(
+        [
+            {
+                "name": "Total en pagos",
+                "amount": total_received_payments,
+                "payment_method_data": True,
+                "sales_data": True,
+            },
+            {
+                "name": "Total en ventas",
+                "amount": total_sales,
+                "sales_data": True,
+                "total_data": True,
+            },
+
+            {
+                "name": "Balanceado",
+                "amount": "Si" if total_sales == total_received_payments else "No",
+                "sales_data": True,
+            },
+            {"name": "Entradas", "amount": total_income, "cashflow_data": True},
+            {
+                "name": "Salidas",
+                "amount": f"-{total_expenses}" if total_expenses != 0 else str(total_expenses),
+                "cashflow_data": True,
+            },
+            {
+                "name": "Total de E/S",
+                "amount": net_cash_flow,
+                "cashflow_data": True,
+                "total_data": True,
+            },
+            {
+                "name": "Total en caja",
+                "amount": net_cash,
+                "total_data": True,
+            },
+            {
+                "name": "Total de ganancias",
+                "amount": total_profit,
+            },
+            {
+                "name": "Total",
+                "amount": total_sales + net_cash_flow,
+                "total_data": True,
+            },
+        ]
+    )
+
+    return cash_summary
