be5b49f3ada81f9438fbe163a50cfaf9e0e92085
David <dmg_92@hotmail.com>
2025-03-12 00:14:02 -0600

    check

--- scripts/check_sales.py
+++ scripts/check_sales.py
@@ -1,24 +1,25 @@
 from sales.models import Sale, ProductSale
 from datetime import date
 from products.models import StoreProduct
+from datetime import date, timedelta
+
 
 def run():
-    today = date.today()
-    sales = Sale.objects.filter(created_at__date=today)
+
+    yesterday = date.today() - timedelta(days=1)
+    sales = Sale.objects.filter(created_at__date=yesterday)
 
     ids = []
     for sale in sales:
-        products_sales = ProductSale.objects.filter(sale=sale).select_related('product')
+        products_sales = ProductSale.objects.filter(sale=sale)
         for product_sale in products_sales:
             if product_sale.price != product_sale.product.unit_price:
-                sp1 = list(StoreProduct.objects.filter(product=product_sale.product)
-                           .values_list('id', flat=True))
+                sp = StoreProduct.objects.filter(product=product_sale.product)
+#                print('sp.pk', sp)
+                sp1 = list(sp.values_list('id', flat=True))
                 ids += sp1
-                
-                sp2 = list(StoreProduct.objects.filter(
-                    store=sale.store,
-                    product__id__in=sp1
-                ).values_list('id', flat=True))
+                sp2 = StoreProduct.objects.filter(store=sale.store, product__id__in=sp)
+                sp2 = list(sp2.values_list('id', flat=True))
                 ids += sp2
 
     ids = set(ids)
