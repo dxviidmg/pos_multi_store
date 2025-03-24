08b215505d9be393bc0a76fd191df4debf722e59
David <dmg_92@hotmail.com>
2025-03-24 11:04:10 -0600

    add role method

--- accounts/models.py
+++ accounts/models.py
@@ -10,16 +10,21 @@ def get_store(self):
     return getattr(StoreWorker.objects.filter(worker=self).first(), "store", None) or Store.objects.filter(manager=self).first()
     
 
+def get_role(self):
+    if Tenant.objects.filter(owner=self).first():
+        return 'owner'
+    if Store.objects.filter(manager=self).first():
+        return 'manager'
+    if StoreWorker.objects.filter(worker=self).first():
+        return 'seller'
+    return 'Sin definir'
+
 def get_tenant(self):
     # Usar `or` para evitar realizar múltiples consultas
     return Tenant.objects.filter(owner=self).first() or self.get_store().tenant
-
-def is_owner(self):
-    # Devolver directamente la evaluación booleana de la consulta
-    return Tenant.objects.filter(owner=self).exists()
 
 # Asignar métodos a la clase `User`
 User.add_to_class("__str__", get_full_name)
 User.add_to_class("get_store", get_store)
 User.add_to_class("get_tenant", get_tenant)
-User.add_to_class("is_owner", is_owner)
+User.add_to_class("get_role", get_role)
