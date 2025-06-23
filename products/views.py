072685134d2d09d795d4976f4badca05e1da0de0
David <dmg_92@hotmail.com>
2025-06-23 14:11:36 -0600

    x

--- products/views.py
+++ products/views.py
@@ -792,7 +792,7 @@ class ProductReassign(APIView):
 class ProductUpperCode(APIView):
 	def post(self, request):
 		tenant = self.request.user.get_tenant()
-		products = Product.objects.filter(brand__tenant=tenant, code__regex=r'^[a-z]+$')#.update(**update_data)
+		products = Product.objects.filter(brand__tenant=tenant, code__regex=r'^[a-z]+$')
 		for product in products:
 			product.code = product.code.upper()
 			product.save()
