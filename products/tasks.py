2fac26a867cab92ca693fda3856c55acfc04651b
David <dmg_92@hotmail.com>
2025-08-02 13:57:00 -0600

    fix

--- products/tasks.py
+++ products/tasks.py
@@ -1,12 +1,10 @@
 from celery import shared_task
 from .models import StoreProduct
-from django.db.models import Sum
 from .serializers import StoreProductForStockSerializer
 
 @shared_task
 def get_store_products_task(store_id):
     queryset = StoreProduct.objects.filter(store_id=store_id)
     serializer = StoreProductForStockSerializer(queryset, many=True)
-    data = serializer.data  # esto es una lista de dicts, lista para serializar
-
-    return data
+    result = serializer.data
+    return result
