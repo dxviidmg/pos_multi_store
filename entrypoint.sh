72e73d40d1d7618beb787cf0e5207c3bc2fe2d84
David <dmg_92@hotmail.com>
2025-10-17 22:42:09 -0600

    echo "=== Iniciando Celery Worker ===" celery -A pos_multi_store worker -l info &

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,9 @@ echo "=== Aplicando migraciones ==="
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 
+echo "=== Iniciando Celery Worker ==="
+celery -A pos_multi_store worker -l info &
+
+
 echo "=== Iniciando Gunicorn ==="
 exec gunicorn pos_multi_store.wsgi:application --timeout 120 --workers 4 --threads 2
