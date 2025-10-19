b6484018f15b8d0ca9e9c5f6985d2d87d914a232
David <dmg_92@hotmail.com>
2025-10-19 16:49:23 -0600

    last log

--- products/tasks.py
+++ products/tasks.py
@@ -3,27 +3,9 @@ from .models import StoreProduct, Store
 from .serializers import StoreProductForStockSerializer, TransferSerializer
 
 @shared_task
-def get_store_products_task(store_id):
-    queryset = StoreProduct.objects.filter(store_id=store_id)
+def get_store_products_task(tenant_id, start_date, end_date):
+
+    queryset = StoreProduct.objects.filter(store_tenant_id=tenant_id)
     serializer = StoreProductForStockSerializer(queryset, many=True)
     data = (serializer.data)
     return data
-
-
-@shared_task
-def calculate_store_investments(tenant):
-    stores = Store.objects.filter(tenant=tenant)
-
-    data = []
-    for store in stores:
-        data.append({"store": store.id, "investment": store.get_investment()})
-
-    return data
-
-
-@shared_task
-def create_transfer_task(data):
-    serializer = TransferSerializer(data=data)
-    serializer.is_valid(raise_exception=True)
-    transfer = serializer.save()
-    return transfer.id
--- products/views.py
+++ products/views.py
@@ -40,11 +40,7 @@ from rest_framework import viewsets
 from django.contrib.auth.models import User
 from .utils import is_list_in_another, is_positive_number
 from logs.models import StoreProductLog
-from .tasks import (
-    get_store_products_task,
-    calculate_store_investments,
-    create_transfer_task,
-)
+
 from django.http import JsonResponse
 from django.db.models import Sum
 
@@ -1101,15 +1097,6 @@ class ImportCanIcludeQuantityView(APIView):
         if store_count == 1:
             return Response(True, status=status.HTTP_200_OK)
         return Response(False, status=status.HTTP_200_OK)
-
-
-#@method_decorator(get_store(), name="dispatch")
-#class StoreProductAsyncView(APIView):
-#    def get(self, request):
-#        tenant = self.request.user.get_tenant()
-#        store = self.request.store
-#        task = get_store_products_task.delay(store.id)
-#        return JsonResponse({"message": "process started", "task_id": task.id})
 
 @method_decorator(get_store(), name="dispatch")
 class StockInOtherStores(APIView):
