d4283337a1370ade513b206d06f2255491a72d77
David <dmg_92@hotmail.com>
2026-03-06 19:19:07 -0600

    x

--- pos_multi_store/urls.py
+++ pos_multi_store/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,9 +30,6 @@ urlpatterns = [
     path('api/', include('printers.urls', namespace="printers")),
     path('api/', include('audit.urls', namespace="audit")),
 ]
-
-from django.conf import settings
-from django.conf.urls.static import static
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
